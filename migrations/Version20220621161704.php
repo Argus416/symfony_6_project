21af8f935182aafb56c324b6f5a13998818b4508
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2022-06-21 18:24:54 +0200

    modifiy text

--- migrations/Version20220621161704.php
+++ migrations/Version20220621161704.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220620071428 extends AbstractMigration
+final class Version20220621161704 extends AbstractMigration
 {
     public function getDescription(): string
     {
